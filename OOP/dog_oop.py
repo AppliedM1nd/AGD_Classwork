9b0c31a46ba4feeb30236ab8d31e46fb62f5a217
aadlam0809 <aadlam0809@highgateschool.org.uk>
2025-10-06 15:54:29 +0100

    Added dog OOP exercise

--- OOP/dog_oop.py
+++ OOP/dog_oop.py
@@ -1,10 +1,19 @@
 class Dog:
+    species = 'Canis familiaris'
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    def speaks(self):
-        return f'{self.name} says "woof, "woof"'
+    def __str__(self):
+        return f'{self.name} is {self.age} years old'
 
-rufus = Dog('Rufus', 8)
+    def speak(self, sound):
+        return f'{self.name} barks {sound}'
+
+class Dachshund(Dog):
+    def speak(self, sound = 'Arf!'):
+        return super().speak(sound)
+
+rufus = Dachshund('Rufus', 8)
 fido = Dog('Fido', 6)
