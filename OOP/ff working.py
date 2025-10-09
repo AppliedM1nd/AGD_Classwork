b65d0f9819ad5387d5eca52a13023cfaf2e7d57a
aadlam0809 <aadlam0809@highgateschool.org.uk>
2025-10-09 10:32:55 +0100

    Added dog OOP exercise

---
+++ OOP/ff working.py
@@ -0,0 +1,21 @@
+import random
+
+def dice_sum(num_dice: int=1,sides: int=6):
+    """ returns the sum of num_dice dice each with sides sides"""
+    total = sum(random.randint(1, sides) for _ in range(num_dice))
+    return total
+
+
+class Character:
+    def __init__(self,name,skill,stamina):
+        self.name = name
+        self.skill = skill
+        self.stamina = stamina
+        self.roll = None
+        self.score = None
+
+    def __repr__(self):
+        return f"Character('{self.name}', Skill={self.skill}, Stamina={self.stamina})"
+
+    def find_score(self,roll,score):
+
