ee993dd39fc7f13df83028afeb76be07540f2a6e
Aadit Lamba <152024706+AppliedM1nd@users.noreply.github.com>
2026-01-31 17:16:03 +0000

    Update CLI.py

--- Databases/Object Relational Mapper/CLI.py
+++ Databases/Object Relational Mapper/CLI.py
@@ -13,17 +13,30 @@ class CLI:
         for activity in activities:
             print(activity)
 
-    def add_activities(self):
+
+    def show_activity_persons(self):
+        activity_name = input('Enter activity name: ')
+        persons = self.controller.get_activity_persons(activity_name)
+        for person in persons:
+            print(person)
+
+    def add_activity(self):
         activity = input('Enter activity name: ')
-        self.controller.activity_names.append(activity)
+        self.controller.insert_activity(activity)
 
-    def add_user_activity(self, first_name, last_name):
-        new_activity = input('Enter activity name: ')
-        self.controller.user.activities.append(new_activity)
+    def add_person(self):
+        first_name = input('Enter first name: ')
+        last_name = input('Enter last name: ')
+        self.controller.insert_person(first_name, last_name)
 
+    def remove_activity(self):
+        activity_name = input('Enter activity name: ')
+        self.controller.remove_activity(activity_name)
 
-
-
-
+    def remove_person(self):
+        first_name = input('Enter first name: ')
+        last_name = input('Enter last name: ')
+        self.controller.remove_person(first_name, last_name)
+        
 if __name__ == '__main__':
     cli = CLI()
